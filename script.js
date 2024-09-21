372a7b8a2ce3b3f6c75ec5994dcf1f4c9d44eacd
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 15:16:37 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+const inputField = document.getElementById('fname');
+
+
+inputField.addEventListener('blur', function() {
+    // Convert the input value to uppercase
+    this.value = this.value.toUpperCase();
+});
